2cdc48c535d4ede98f3573d804bcd7083b7eb689
alwell-kevin <alwell-kevin@github.com>
2020-04-22 12:55:14 -0400

    add fibonacci endpoint

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.all('/number', (req, resp) => {
   resp.end(JSON.stringify(sequence));
 });
 
+app.all('/fibonacci', (req, resp) => {
+  console.log(req, "resp", resp);
+
+  resp.setHeader('Content-Type', 'application/json');
+  resp.end(JSON.stringify("A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc."));
+});
+
 // Start server
 app.listen(port, () => {
     console.log('Express server started on port ' + port);
