cf744665b35869af3396bb0af9c9308aefadf08c
Kevin Alwell <alwell-kevin@github.com>
2020-05-08 11:37:26 -0400

    Create vulnerableJS.js

---
+++ vulnerableJS.js
@@ -0,0 +1,25 @@
+function printLines () {
+  console.log('This is a sentence.')
+  console.log('Hey, heres another one')
+}
+
+function blowThisUp (secret) {
+  eval secret
+}
+
+function emptyFunction() {
+  
+}
+
+function emptyIfBlock () {
+  var secret = true
+  var test = true
+  if (test) {
+  }
+}
+
+var link = '<a href="http://example.com" target="_blank">Example</a>';
+
+printLines()
+emptyMethod()
+emptyIfBlock()
